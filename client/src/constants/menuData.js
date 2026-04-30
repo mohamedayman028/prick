ec5502c08e102297ddf5e4f4230a637e949cac9a
mohamedayman028 <mohamedayman02h@gmail.com>
2026-04-30 16:25:29 +0300

    ااا

--- client/src/constants/menuData.js
+++ client/src/constants/menuData.js
@@ -6,80 +6,69 @@ export const FALLBACK_CATEGORIES = [
         products: [
             {
                 id: 135,
-                name: "Classic Mojito | موهيتو كلاسيك",
-                description_ar: "مشروب موهيتو منعش مع الليمون والنعناع والثلج.",
-                imageUrl: "default.jpg",
-                items: [
-                    { size: "S", price: 65 },
-                    { size: "M", price: 75 },
-                    { size: "L", price: 85 }
-                ]
-            },
-            {
-                id: 136,
                 name: "Strawberry Mojito | موهيتو فراولة",
                 description_ar: "موهيتو منعش بنكهة الفراولة.",
                 imageUrl: "mojito-placeholder.png",
                 items: [{ size: "S", price: 79 }, { size: "M", price: 84 }, { size: "L", price: 89 }]
             },
             {
-                id: 137,
+                id: 136,
                 name: "Blueberry Mojito | موهيتو توت",
                 description_ar: "موهيتو منعش بنكهة التوت.",
                 imageUrl: "mojito-placeholder.png",
                 items: [{ size: "S", price: 79 }, { size: "M", price: 84 }, { size: "L", price: 89 }]
             },
             {
-                id: 138,
+                id: 137,
                 name: "Pineapple Mojito | موهيتو أناناس",
                 description_ar: "موهيتو منعش بنكهة الأناناس.",
                 imageUrl: "mojito-placeholder.png",
                 items: [{ size: "S", price: 79 }, { size: "M", price: 84 }, { size: "L", price: 89 }]
             },
             {
-                id: 139,
+                id: 138,
                 name: "Mango Mojito | موهيتو مانجا",
                 description_ar: "موهيتو منعش بنكهة المانجو.",
                 imageUrl: "mojito-placeholder.png",
                 items: [{ size: "S", price: 79 }, { size: "M", price: 84 }, { size: "L", price: 89 }]
             },
             {
-                id: 140,
+                id: 139,
                 name: "Peach Mojito | موهيتو خوخ",
                 description_ar: "موهيتو منعش بنكهة الخوخ.",
                 imageUrl: "mojito-placeholder.png",
                 items: [{ size: "S", price: 79 }, { size: "M", price: 84 }, { size: "L", price: 89 }]
             },
             {
-                id: 141,
+                id: 140,
                 name: "Mix Berry Mojito | موهيتو ميكس بيري",
                 description_ar: "موهيتو منعش بنكهة ميكس بيري.",
                 imageUrl: "mojito-placeholder.png",
                 items: [{ size: "S", price: 79 }, { size: "M", price: 84 }, { size: "L", price: 89 }]
             },
             {
-                id: 142,
+                id: 141,
                 name: "Kiwi Mojito | موهيتو كيوي",
                 description_ar: "موهيتو منعش بنكهة الكيوي.",
                 imageUrl: "mojito-placeholder.png",
                 items: [{ size: "S", price: 79 }, { size: "M", price: 84 }, { size: "L", price: 89 }]
             },
             {
-                id: 143,
+                id: 142,
                 name: "Passion Fruit Mojito | موهيتو باشون",
                 description_ar: "موهيتو منعش بنكهة الباشون فروت.",
                 imageUrl: "mojito-placeholder.png",
                 items: [{ size: "S", price: 79 }, { size: "M", price: 84 }, { size: "L", price: 89 }]
             },
             {
-                id: 144,
+                id: 143,
                 name: "Apple Mojito | موهيتو تفاح",
                 description_ar: "موهيتو منعش بنكهة التفاح.",
                 imageUrl: "mojito-placeholder.png",
                 items: [{ size: "S", price: 79 }, { size: "M", price: 84 }, { size: "L", price: 89 }]
             },
             {
-                id: 145,
+                id: 144,
                 name: "Raspberry Mojito | موهيتو راس بيري",
                 description_ar: "موهيتو منعش بنكهة الراس بيري.",
                 imageUrl: "mojito-placeholder.png",
